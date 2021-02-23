d1071468d245ddded4ed8d5bcc86d7bbc82cd5de
Tanviras <tanvirasme@gmail.com>
2021-02-23 12:44:19 +0600

    signed-out

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
 import './App.css';
+import firebase from "firebase/app";
+import React, { useState } from 'react';
+import "firebase/analytics";
+import "firebase/auth";
+import firebaseConfig from './firebaseConfig';
+
+firebase.initializeApp(firebaseConfig);
 
 function App() {
+const [user,setUser]=useState({displayName:'',email:'',photoURL:'',isSignedIn:'false'});
+
+
+  const provider = new firebase.auth.GoogleAuthProvider();//copied
+
+
+
+//sign-in-button
+  const handleSignIn=()=>{
+  firebase.auth().signInWithPopup(provider)//copied
+
+    .then((result) => {
+    const {displayName,email,photoURL}=result.user;
+
+    const signedInUser={//destructure
+      isSignedIn: true,
+      displayName: displayName,
+      email: email,
+      photoURL: photoURL
+    }
+
+   setUser(signedInUser);
+  //  console.log(signedInUser);//tahole amar sign-in kora email,name,email-dp dekhabe
+
+   }).catch((error) => {
+    var errorMessage = error.message;
+    console.log(errorMessage);
+    });
+    }
+//sign-in-button-end
+
+
+//sign-out button
+const handleSignOut=()=>{
+  firebase.auth().signOut()//signOut() is a method,not made by me
+  .then(result=>{
+                const signedOutUser={//destructure
+                  isSignedIn: false,
+                  displayName: '',
+                  email: '',
+                  photoURL: ''
+                }
+  setUser(signedOutUser);
+
+  }).catch((error)=> {
+    var errorMessage = error.message;
+    console.log(errorMessage);
+    });
+  
+}
+//sign-out-button-end
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {/* if-else-in-one-line */}
+      {/* toggling between sign-in and sign-out */}
+      {
+      user.isSignedIn? <button onClick={handleSignOut}>Sign out</button>:
+                        <button onClick={handleSignIn}>Sign in</button>
+      }
+     
+     {/* if signed-in ,then show these */}
+      {
+        user.isSignedIn && <div> 
+                                <p>Welcome {user.displayName}</p>
+                                <p>E-mail:{user.email}</p>
+                                <img src={user.photoURL} alt=""></img>
+                                </div>
+      }
+
+
     </div>
   );
 }
